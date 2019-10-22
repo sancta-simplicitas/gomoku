c153e82ba1137c91a70db22d8da6ff1945932048
hzqd <hzqdfs@gmail.com>
2019-10-22 16:02:15 +0800

    bug commit

--- src/main/kotlin/ChessBoard.kt
+++ src/main/kotlin/ChessBoard.kt
@@ -2,10 +2,15 @@ import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.Toolkit
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import javax.swing.JPanel
 
 class ChessBoard : JPanel() {
     private val img = Toolkit.getDefaultToolkit().getImage("img/board.png")
+    private var chessList = arrayListOf<Chess>()
+    private var chessCount = 0
+    private var isBlack = true
 
     override fun paintComponent(g: Graphics) {
         super.paintComponent(g)
@@ -25,10 +30,51 @@ class ChessBoard : JPanel() {
             fillRect(MARGIN + (ROCO / 2) * SPAN - 2, MARGIN + (ROCO - 3) * SPAN - 2, 5, 5)
             fillRect(MARGIN + (ROCO - 3) * SPAN - 2, MARGIN + (ROCO - 3) * SPAN - 2, 5, 5)
             //画棋子:
-            Chess(2,1, Color.BLACK).draw(this)
-            Chess(5,2, Color.WHITE).draw(this)
+            (0 until chessCount).forEach {
+                chessList[it].draw(this)
+                if (it == chessCount - 1) { // 如果是最后一个棋子
+                    color = Color.red
+                    drawRect( //下一行: xPos, 下下行: yPos //- DIAMETER / 2
+                        (chessList[it].col * SPAN + MARGIN - DIAMETER / 2).toInt(),
+                        (chessList[it].row * SPAN + MARGIN - DIAMETER / 2).toInt(),
+                        DIAMETER.toInt(),
+                        DIAMETER.toInt()
+                    )
+                }
+            }
         }
     }
 
     override fun getPreferredSize() = Dimension(MARGIN * 2 + SPAN * ROCO, MARGIN * 2 + SPAN * ROCO)
+
+    class MouseMonitor : MouseAdapter() {
+        override fun mousePressed(e: MouseEvent) = with(e) {
+            (x - MARGIN + SPAN / 2) / SPAN to (y - MARGIN + SPAN / 2) / SPAN
+        }.run {
+            // 将鼠标单击的像素坐标转换成网格索引
+            val col = first
+            val row = second
+            // 落在棋盘外不能下棋
+            if (col < 0 || col > ROCO || row < 0 || row > ROCO) return
+            // 已经有棋子存在的x,y不能下棋
+            with(ChessBoard()) {
+                fun hasChess(col: Int, row: Int) = run {
+                    for (i in 0 until chessCount) {
+                        val ch = listOf<Chess>()[i]
+                        if (ch.col == col && ch.row == row) return true
+                    }
+                    false
+                }
+                if (hasChess(col, row)) return
+                Chess(col, row, if (isBlack) Color.BLACK else Color.WHITE).let {
+                    chessList[chessCount++] = it
+                    repaint()
+                    isBlack = !isBlack
+                }
+            }
+        }
+    }
+    init {
+        addMouseListener(MouseMonitor())
+    }
 }
